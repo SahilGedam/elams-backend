eb8a094c4bcaf5b6917c5f432523a176c06069db
Sahil <sahil.gedam@autoflowtech.com>
2026-02-04 23:32:48 +0530

    ELAMS initialized

---
+++ src/main/java/com/elams/elams_backend/ElamsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elams.elams_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElamsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElamsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elams/elams_backend/ElamsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elams.elams_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElamsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
