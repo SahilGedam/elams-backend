8a05fc09b0447eb1229112bf5d63eca0135652f6
Sahil <sahil.gedam@autoflowtech.com>
2026-02-07 22:41:30 +0530

    Test API

---
+++ src/main/java/com/elams/elams_backend/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.elams.elams_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping
+    public String test() {
+        return "JWT Auth Working";
+    }
+}
+
